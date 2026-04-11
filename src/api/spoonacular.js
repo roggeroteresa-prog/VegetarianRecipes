45187ae6fa6db8136b96b5a9062dfaf0fedcf5ee
roggeroteresa-prog <roggeroteresa@gmail.com>
2026-04-11 12:26:17 +0200

    Format and clean up spoonacular.js

--- src/api/spoonacular.js
+++ src/api/spoonacular.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiKey ="4639e48265ab468f8bd2406fd66e5b5a";// process.env.REACT_APP_SPOONACULAR_KEY;
+const apiKey ="4639e48265ab468f8bd2406fd66e5b5a"; // process.env.REACT_APP_SPOONACULAR_KEY;
 
 export const api = axios.create({
   baseURL: "https://api.spoonacular.com/recipes",
