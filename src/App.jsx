f03f6d8a9c67208ac96417e2745a2ed5152e4999
roggeroteresa-prog <roggeroteresa@gmail.com>
2026-04-11 10:46:59 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FavoritesProvider } from "./context/FavoritesContext";
+
 import Home from "./pages/Home";
 import Results from "./pages/Results";
 import RecipeDetails from "./pages/RecipeDetails";
@@ -7,16 +9,18 @@ import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
-    <BrowserRouter basename="/VegetarianRecipes">
-      <Navbar />
-      <main className="app-main">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/results" element={<Results />} />
-          <Route path="/recipe/:id" element={<RecipeDetails />} />
-          <Route path="/favorites" element={<Favorites />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <FavoritesProvider>
+      <BrowserRouter basename="/VegetarianRecipes">
+        <Navbar />
+        <main className="app-main">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/results" element={<Results />} />
+            <Route path="/recipe/:id" element={<RecipeDetails />} />
+            <Route path="/favorites" element={<Favorites />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </FavoritesProvider>
   );
 }
